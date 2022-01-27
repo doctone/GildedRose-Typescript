389ebb20472ccb8148745acec8f9cf2d7ec149c1
Sam James <Sam.James@softwire.com>
2022-01-27 11:27:24 +0000

    added first master test

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -3,10 +3,39 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+    it('should correspond with golden record output', function () {
+        const expectedItems: Array<Item> = [
+            new Item('item1', 2, 5),
+            new Item('item2', 1, 12),
+            new Item('item3', 1, 2),
+            new Item('Aged Brie', 7, 6),
+            new Item('Aged Brie', 2, 38),
+            new Item('Sulfuras, Hand of Ragnaros', 20, 20),
+            new Item('Sulfuras, Hand of Ragnaros', 80, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 8, 13),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 14),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 1, 0)
+
+        ];
+        const testItems: Array<Item> = [
+            new Item('item1', 5, 8),
+            new Item('item2', 4, 15),
+            new Item('item3', 2, 6),
+            new Item('Aged Brie', 10, 3),
+            new Item('Aged Brie', 1, 33),
+            new Item('Sulfuras, Hand of Ragnaros', 20, 20),
+            new Item('Sulfuras, Hand of Ragnaros', 80, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 11, 8),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 8, 8),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 2, 8),
+        ]
+
+        const gildedRose = new GildedRose(testItems)
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+
+        gildedRose.items.should.deep.equal(expectedItems);
     });
 
 });
--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -1,3 +1,23 @@
 import { Item, GildedRose } from '../app/gilded-rose';
 
 // Add a master test here
+
+const items: Array<Item> = [
+    new Item('item1', 5, 8),
+    new Item('item2', 4, 15),
+    new Item('item3', 2, 6),
+    new Item('Aged Brie', 10, 3),
+    new Item('Aged Brie', 1, 33),
+    new Item('Sulfuras, Hand of Ragnaros', 20, 20),
+    new Item('Sulfuras, Hand of Ragnaros', 80, 20),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 11, 8),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 8, 8),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 2, 8),
+]
+
+const gildedRose = new GildedRose(items);
+gildedRose.updateQuality();
+gildedRose.updateQuality();
+gildedRose.updateQuality();
+
+console.log(gildedRose.items);
